1dd0bef2384c1aea7c39950b6193e4e86dbe6c98
Matt Flinn <mflinn@fool.com>
2013-10-13 21:28:07 -0400

    Latest changes...

--- js/fool-mobile.js
+++ js/fool-mobile.js
@@ -1,9 +1,16 @@
-$(function() {
-        
-    // Bind the swipeleftHandler callback function to the swipe event on div.box
-  $(document).on("swiperight", "div.swipeToClear", function() {
-    $( event.target ).hide();
-  }); 
-  
-        
+$(function() {    
+    $(document).on("swiperight", "div.swipeToClear", function() {
+        $( event.target ).hide();
+    });        
+
+    $(document).on("change", "#selectHomeScreen", function() {
+        text = $(this).text();
+        if (text == "Trade Alerts") {
+            $('homeScreenSelector').css("background-color", "#BB0000");
+        } else if (text == "Latest News") {
+            $('homeScreenSelector').css("background-color", "#55AA22");
+        } else if (text == "My Stocks") {
+            $('homeScreenSelector').css("background-color", "#0077AA");
+        }
+    });
 });
